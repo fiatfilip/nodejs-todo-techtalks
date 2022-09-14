3a88cbe9f205df31689c22326ae8e22effc7c7b9
Filip Fiat <filip.fiat@haufe-lexware.com>
2022-09-14 13:42:20 +0300

    use 8080 instead of 3000 https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/java-tomcat-proxy.html?icmpid=docs_elasticbeanstalk_console

--- index.js
+++ index.js
@@ -40,6 +40,6 @@ app.get("/", async function(req, res) {
 });
 
 //set app to listen on port 3000
-app.listen(3000, function() {
-    console.log("server is running on port 3000");
+app.listen(8080, function() {
+    console.log("server is running on port 8080");
 });
