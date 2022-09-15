cd7d36a094eb2865778bcedfb91c20323e3a8647
Filip Fiat <filip.fiat@haufe-lexware.com>
2022-09-15 17:56:16 +0300

    start the presentation with initial app

--- index.js
+++ index.js
@@ -8,38 +8,41 @@ app.set("view engine", "ejs");
 //render css files
 app.use(express.static("public"));
 
-const db = require('./model/queries')
+//placeholders for added task
+var task = ["buy socks", "practise with nodejs"];
+//placeholders for removed task
+var complete = ["finish jquery"];
 
 //post route for adding new task 
 app.post("/addtask", function(req, res) {
     var newTask = req.body.newtask;
     //add the new task from the post route
-    db.addTodo(newTask)
+    task.push(newTask);
     res.redirect("/");
 });
 
-app.post("/removetask", async function(req, res) {
+app.post("/removetask", function(req, res) {
     var completeTask = req.body.check;
     //check for the "typeof" the different completed task, then add into the complete task
     if (typeof completeTask === "string") {
-        await db.completeTodo(completeTask);
+        complete.push(completeTask);
+        //check if the completed task already exits in the task when checked, then remove it
+        task.splice(task.indexOf(completeTask), 1);
     } else if (typeof completeTask === "object") {
         for (var i = 0; i < completeTask.length; i++) {
-            await db.completeTodo(completeTask[i]);
-            completeTask.splice(completeTask.indexOf(completeTask[i]), 1);
+            complete.push(completeTask[i]);
+            task.splice(task.indexOf(completeTask[i]), 1);
         }
     }
     res.redirect("/");
 });
 
 //render the ejs and display added task, completed task
-app.get("/", async function(req, res) {
-    const openTasks = await db.getTodos(true) 
-    const completedTasks = await db.getTodos(false)
-    res.render("index", { task: openTasks, complete: completedTasks });
+app.get("/", function(req, res) {
+    res.render("index", { task: task, complete: complete });
 });
 
-//set app to listen on port 3000
+//set app to listen on port 8080
 app.listen(8080, function() {
-    console.log("server is running on port 8080");
+    console.log("server is running on port ", 8080);
 });
